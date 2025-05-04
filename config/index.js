9e3dcfe68ca604f264fb6eac3b415869cd87a8ad
Gurnoor Singh <190388566+terabyte-trifler@users.noreply.github.com>
2025-05-04 20:40:43 +0530

    Contact Me started working

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@
 
 // EMAILJS Config Data
 
-export const EMAILJS_TEMPLATE_ID = "template_kh9buql"
+export const EMAILJS_TEMPLATE_ID = "contact_form"
 
-export const EMAILJS_SERVICE_ID = "service_hd5qbz2"
+export const EMAILJS_SERVICE_ID = "contact_service"
 
-export const EMAILJS_PUBLIC_KEY = "user_SXwbm2bfwtluGBhARcTaf"
+export const EMAILJS_PUBLIC_KEY = "kbUBDsDzqdUa_vXrG"
