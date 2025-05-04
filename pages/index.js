9640b1c44ed00295f158eecd1a862ca1880e6c9c
Gurnoor Singh <190388566+terabyte-trifler@users.noreply.github.com>
2025-05-04 15:57:13 +0530

    added major changes

--- pages/index.js
+++ pages/index.js
@@ -27,13 +27,12 @@ export default function HomePage() {
 import { Layout } from "../components";
 
 
-
 export default function HomePage(){
 
     return (
         <div>
             <Layout>
-                
+                <h1>{process.env.NAME}</h1>
             </Layout>
         </div>
 >>>>>>> 868306c (first commit)
